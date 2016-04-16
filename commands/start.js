c86f272d60be49880d24f4dc26b345c42650a9f2
MooYeol Prescott Lee <mooyoul@gmail.com>
2016-04-17 06:39:48 +0900

    Added Developer information into `/start` command

--- commands/start.js
+++ commands/start.js
@@ -41,7 +41,15 @@ const
 module.exports = exports = (bot) => {
   bot.onText(/\/start/, (message) => {
     findOrCreateUser(message.from).then(() => {
-      bot.sendMessage(message.from.id, '안녕하세요! 다음 명령어로 RSS봇을 시작해보세요!', {
+      bot.sendMessage(message.from.id, [
+        '안녕하세요! RSS봇을 사용해주셔서 감사합니다.',
+        '',
+        '본 RSS봇은 무료로 제공되는 로봇입니다.',
+        '개발자 Telegram: @mooyoul',
+        '개발자가 만든 좋은 변호사 찾는 로톡: https://www.lawtalk.co.kr/tg1',
+        '',
+        '다음 명령어로 RSS봇을 시작해보세요!'
+      ].join('\n'), {
         reply_markup: JSON.stringify({
           one_time_keyboard: true,
           keyboard: [
