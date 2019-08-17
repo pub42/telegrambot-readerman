8e9991b87ac2f9967b09eb127b586df771cbcf03
xziy <cziy@ya.ru>
2019-08-17 13:29:21 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,21 +4,27 @@
 module.exports = exports = (bot) => {
   bot.onText(/\/help/i, (message) => {
     bot.sendMessage(message.from.id, [
-      'RSS봇에는 다음 명령들이 있습니다.',
+      'RSS reader',
       '',
       '/subscribe [URL]',
-      '- 지정된 RSS 피드를 구독합니다.',
+      '- Подписатся на канал.',
+      '- Subscribe to the specified RSS feed.',
       '',
       '/unsubscribe [URL|FEED_NUMBER]',
-      '- 지정된 RSS 피드를 구독해제합니다.',
+      '- Удалить канал',
+      '- Remove the specified RSS feed.',
       '',
       '/list',
-      '- 구독 중인 RSS 피드 목록을 가져옵니다.',
+      '- Список всех каналов',
+      '- List all known RSS feed',
       '',
       '/read [URL|FEED_NUMBER]',
-      '- 지정한 RSS 피드의 최신글을 즉시 가져옵니다. 피드가 입력되지 않으면, 전체 피드를 가져옵니다.',
+      'Получить последние сообщения из указанного RSS-канала немедленно. Если канал не введен, берется весь канал.',
+      '- Receive the latest posts from the specified RSS feed immediately. If the channel is not entered, the entire channel is taken.',
+      '',
       '/help',
-      '- 이 메세지를 출력합니다.'
+      '- Это сообщение',
+      '- This message'
     ].join('\n'));
   });
 };
