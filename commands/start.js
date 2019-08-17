3bbf7aa8694c86d5ca9c3726b7df0ff5ac287be5
xziy <cziy@ya.ru>
2019-08-17 13:25:14 +0300

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -42,14 +42,11 @@ module.exports = exports = (bot) => {
   bot.onText(/\/start/, (message) => {
     findOrCreateUser(message.from).then(() => {
       bot.sendMessage(message.from.id, [
-        '안녕하세요! RSS봇을 사용해주셔서 감사합니다.',
+        'Welcome to https://42team.org',
         '',
-        '본 RSS봇은 무료로 제공되는 로봇입니다.',
-        '개발자 Telegram: @mooyoul',
-        '개발자가 만든 좋은 변호사 찾는 로톡: https://www.lawtalk.co.kr/tg1',
-        'RSS봇 Github: https://github.com/mooyoul/telegrambot-readerman',
-        '',
-        '다음 명령어로 RSS봇을 시작해보세요!'
+        'Please look video for setup notifer',
+        'https://link.m42.cx/notifer_setup',
+        'Goodluck',
       ].join('\n'), {
         reply_markup: JSON.stringify({
           one_time_keyboard: true,
